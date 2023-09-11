aca57d252d24f55bbbaf1f0714139bf60fc468bf
elliott <130366417+toorques@users.noreply.github.com>
2023-09-11 08:48:18 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,1 +1,14 @@
-console.log("O arquivo separando está OK!");
+
+//------------------------------------------------------------------
+//Abaixo começa do javascript do site.
+
+//Alteração do título
+//console.log(document);
+//Cria a variável título e armazena o contéudo da classe
+let titulo = document.querySelector(".titulo");
+//Mostra no devtools o conteudo de texto
+//console.log(titulo);
+//Atribuição de um novo valor na variavel título
+titulo.textContent = "Banana!!!!"
+
+//Alteração da tabela
